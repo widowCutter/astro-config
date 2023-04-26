d25bb53c08318d7ef387b304fdc25930f95eba93
widowCutter <ondrej@pribyl.tech>
2023-04-26 22:15:07 +0200

    added make run

--- plugins/user.lua
+++ plugins/user.lua
@@ -3,6 +3,13 @@ return {
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
   -- {
+      "Pocco81/auto-save.nvim",
+    	config = function()
+    		 require("auto-save").setup {
+    			-- your config goes here
+    			-- or just leave it empty :)
+    		 }
+    	end,
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
   --   config = function()
